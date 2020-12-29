4356ef709d6355626bf42931daeba7f6f42721a9
mithhu <mahamudul.hasan@evaly.com.bd>
2020-12-29 23:21:43 +0600

    Session Authentication

--- LIREDDIT-SERVER/src/resolvers/user.ts
+++ LIREDDIT-SERVER/src/resolvers/user.ts
@@ -6,6 +6,7 @@ import {
   Arg,
   Ctx,
   ObjectType,
+  Query,
 } from "type-graphql";
 import argon2 from "argon2";
 import { User } from "../entities/User";
@@ -37,10 +38,21 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
+  @Query(() => User, { nullable: true })
+  async me(@Ctx() { req, em }: MyContext) {
+    // you are not logged in
+    if (!req.session.userId) {
+      return null;
+    }
+
+    const user = await em.findOne(User, { id: req.session.userId });
+    return user;
+  }
+
   @Mutation(() => UserResponse)
   async register(
     @Arg("options") options: UsernamePasswordInput,
-    @Ctx() { em }: MyContext
+    @Ctx() { em, req }: MyContext
   ): Promise<UserResponse> {
     if (options.username.length <= 2) {
       return {
@@ -83,13 +95,15 @@ export class UserResolver {
         };
       }
     }
+
+    req.session.userId = user.id;
     return { user };
   }
 
   @Mutation(() => UserResponse)
   async login(
     @Arg("options") options: UsernamePasswordInput,
-    @Ctx() { em }: MyContext
+    @Ctx() { em, req }: MyContext
   ): Promise<UserResponse> {
     const user = await em.findOne(User, { username: options.username });
     if (!user) {
@@ -114,6 +128,7 @@ export class UserResolver {
       };
     }
 
+    req.session.userId = user.id;
     return { user };
   }
 }
