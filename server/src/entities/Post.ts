ac643ebbaee77ee890906d90ce5a78635256499c
mithhu <mahamudul.hasan@evaly.com.bd>
2021-01-06 00:50:30 +0600

    many to one

--- server/src/entities/Post.ts
+++ server/src/entities/Post.ts
@@ -1,12 +1,14 @@
 import { ObjectType, Field } from "type-graphql";
 import {
+  Entity,
+  Column,
   PrimaryGeneratedColumn,
   CreateDateColumn,
   UpdateDateColumn,
-  Column,
-  Entity,
   BaseEntity,
+  ManyToOne,
 } from "typeorm";
+import { User } from "./User";
 
 @ObjectType()
 @Entity()
@@ -15,15 +17,30 @@ export class Post extends BaseEntity {
   @PrimaryGeneratedColumn()
   id!: number;
 
+  @Field()
+  @Column()
+  title!: string;
+
+  @Field()
+  @Column()
+  text!: string;
+
+  @Field()
+  @Column({ type: "int", default: 0 })
+  points!: number;
+
+  @Field()
+  @Column()
+  creatorId: number;
+
+  @ManyToOne(() => User, (user) => user.posts)
+  creator: User;
+
   @Field(() => String)
   @CreateDateColumn()
   createdAt: Date;
 
   @Field(() => String)
   @UpdateDateColumn()
-  updatedAt = Date;
-
-  @Field()
-  @Column({ type: "text" })
-  title!: string;
+  updatedAt: Date;
 }
